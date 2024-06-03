8890ebc0d467f9408fdc9a353afed8b349b4052e
Maren <marenehlers@gmx.net>
2024-06-03 17:18:50 +0200

    creating Form.jsx with form, inputfields, button and importing to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
+import Form from "./Components/Form/Form.jsx";
 
 function App() {
   return (
     <>
       <h1>Theme Creator</h1>
-      <form></form>
+      <Form />
+
       {initialColors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
