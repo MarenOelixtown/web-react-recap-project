35552b02f86a1b34bfe8e335bacb84543e6239bc
Maren <marenehlers@gmx.net>
2024-06-04 15:03:36 +0200

    creating Button-Component, importing to Color-Component and adding Cancel-Button

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,4 +1,5 @@
 import "./Color.css";
+import Button from "./Button.jsx";
 
 export default function Color({ color }) {
   return (
@@ -12,6 +13,7 @@ export default function Color({ color }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+      <Button text="Cancel" />
     </div>
   );
 }
