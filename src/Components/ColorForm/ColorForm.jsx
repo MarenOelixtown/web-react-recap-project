bf88d2227cbf79a41750eb3a2e7e0ae1db9a8d11
Maren <marenehlers@gmx.net>
2024-06-04 11:14:34 +0200

    handle color-change and and synchronosize with value from inputfield

--- src/Components/ColorForm/ColorForm.jsx
+++ src/Components/ColorForm/ColorForm.jsx
@@ -1,22 +1,45 @@
 import "./ColorForm.css";
+import { useState } from "react";
 
 export default function ColorForm() {
+  const [colorHex, setColorHex] = useState("#e5430f");
+  const [colorContrastHex, setColorContrastHex] = useState("#FFFFFF");
+
+  const handleColorChangeOne = (event) => {
+    setColorHex(event.target.value);
+  };
+  const handleColorChangeTwo = (event) => {
+    setColorContrastHex(event.target.value);
+  };
+
   return (
     <form className="form-container">
       <label htmlFor="role">Role</label>
       <input type="text" name="role" id="role" />
       <label htmlFor="hex-text">Hex</label>
-      <input type="text" name="hex-text" id="hex-text" />
+      <input type="text" name="hex-text" id="hex-text" value={colorHex} />
       <label htmlFor="hex"></label>
-      <input type="color" name="hex" id="hex" value="#e5430f" />
+      <input
+        type="color"
+        name="hex"
+        id="hex"
+        value={colorHex}
+        onChange={handleColorChangeOne}
+      />
       <label htmlFor="contrastText">Contrast Text</label>
-      <input type="text" name="contrastText" id="contrastText" />
+      <input
+        type="text"
+        name="contrastText"
+        id="contrastText"
+        value={colorContrastHex}
+      />
       <label htmlFor="hex-contrast"></label>
       <input
         type="color"
         name="hex-contrast"
         id="hex-contrast"
-        value="#FFFFFF"
+        value={colorContrastHex}
+        onChange={handleColorChangeTwo}
       />
       <button type="submit">Add Color</button>
     </form>
